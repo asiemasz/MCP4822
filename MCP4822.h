61e3ff438fe4adcfecb85d04de7df79607adbc59
Adam Siemasz <asiemasz1@gmail.com>
2021-10-28 23:21:58 +0200

    Add basic enums and structs

---
+++ MCP4822.h
@@ -0,0 +1,36 @@
+#ifndef MCP4822_H
+#define MCP4822_H
+
+#include <stdint.h>
+
+typedef enum {
+    MCP4822_DAC_A,
+    MCP4822_DAC_B
+} MCP4822_OUTPUT;
+
+typedef enum {
+    MCP4822_OUTPUT_GAIN_x2,
+    MCP4822_OUTPUT_GAIN_x1
+} MCP4822_OUTPUT_GAIN;
+
+typedef enum {
+    MCP4822_OUTPUT_POWERDOWN_CONTROL_BUFFER_DIS,
+    MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT
+} MCP4822_OUTPUT_POWERDOWN_CONTROL;
+
+typedef struct {
+    MCP4822_OUTPUT                  output;
+    MCP4822_OUTPUT_GAIN              gain;
+    MCP4822_OUTPUT_POWERDOWN_CONTROL powerDown;
+} MCP4822_OUTPUT_CONFIG;
+
+typedef void (*MCP4822_regWrite)(const struct MCP4822_device, void* data, uint8_t len);
+
+typedef struct {
+    const uint8_t           SS_pin;
+    const uint32_t*         SS_Port;
+    const MCP4822_regWrite  writeFunction;
+    const float             refVoltage;
+} MCP4822_device;
+
+#endif
