64559697da4e339b9694835e0caa7f1dbc8c1f5f
Adam Siemasz <asiemasz1@gmail.com>
2021-10-29 16:42:54 +0200

    Add chip select functions

--- MCP4822.h
+++ MCP4822.h
@@ -20,19 +20,25 @@ typedef enum {
     MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT
 } MCP4822_OUTPUT_POWERDOWN_CONTROL;
 
-typedef struct {
+typedef union {
     MCP4822_OUTPUT                   output;
+    uint8_t                          res;
     MCP4822_OUTPUT_GAIN              gain;
     MCP4822_OUTPUT_POWERDOWN_CONTROL powerDown;
+    uint8_t                          _config;
 } MCP4822_OUTPUT_CONFIG;
 
-typedef void (*MCP4822_regWrite)(const struct MCP4822_device dev, void* data,const uint8_t len);
+typedef void (*MCP4822_regWrite) (const struct MCP4822_device dev, void* data,const uint8_t len);
+typedef void (*MCP4822_CSenable) (const struct MCP4822_device dev, uint32_t* GPIO_port, uint8_t GPIO_pin);
+typedef void (*MCP4822_CSdisable) (const struct MCP4822_device dev, uint32_t* GPIO_port, uint8_t GPIO_pin);
 
 
 typedef struct {
     const uint8_t           SS_pin;
     const uint32_t*         SS_Port;
     const MCP4822_regWrite  writeFunction;
+    const MCP4822_CSenable  CSenableFunction;
+    const MCP4822_CSenable  CSdisableFunction;
 } MCP4822_device;
 
 void MCP4822_setValue(const struct MCP4822_device, uint16_t value, MCP4822_OUTPUT_CONFIG config);
