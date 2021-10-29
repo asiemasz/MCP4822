ff4ec06b469fd6f7a8eb8dd31fbf7ecf1bfdda70
Adam Siemasz <asiemasz1@gmail.com>
2021-10-29 17:04:43 +0200

    Make configuration an enum to easify register writing. Correct some functions declarations

--- MCP4822.h
+++ MCP4822.h
@@ -21,17 +21,18 @@ typedef enum {
 } MCP4822_OUTPUT_POWERDOWN_CONTROL;
 
 typedef union {
-    MCP4822_OUTPUT                   output;
-    uint8_t                          res;
-    MCP4822_OUTPUT_GAIN              gain;
-    MCP4822_OUTPUT_POWERDOWN_CONTROL powerDown;
     uint8_t                          _config;
+    struct {
+        uint8_t powerDown : 1;
+        uint8_t gain : 1;
+        uint8_t res : 1;
+        uint8_t output : 1;
+    };
 } MCP4822_OUTPUT_CONFIG;
 
-typedef void (*MCP4822_regWrite) (const struct MCP4822_device dev, void* data,const uint8_t len);
-typedef void (*MCP4822_CSenable) (const struct MCP4822_device dev, uint32_t* GPIO_port, uint8_t GPIO_pin);
-typedef void (*MCP4822_CSdisable) (const struct MCP4822_device dev, uint32_t* GPIO_port, uint8_t GPIO_pin);
-
+typedef void (*MCP4822_regWrite) (uint8_t* data,const uint8_t len);
+typedef void (*MCP4822_CSenable) (uint32_t* GPIO_port, uint8_t GPIO_pin);
+typedef void (*MCP4822_CSdisable) (uint32_t* GPIO_port, uint8_t GPIO_pin);
 
 typedef struct {
     const uint8_t           SS_pin;
@@ -41,9 +42,9 @@ typedef struct {
     const MCP4822_CSenable  CSdisableFunction;
 } MCP4822_device;
 
-void MCP4822_setValue(const struct MCP4822_device, uint16_t value, MCP4822_OUTPUT_CONFIG config);
+void MCP4822_setValue(const MCP4822_device dev, uint16_t value, MCP4822_OUTPUT_CONFIG config);
 
-void MCP4822_setVoltage(const struct MCP4822_device, float value, MCP4822_OUTPUT_CONFIG config);
+void MCP4822_setVoltage(const MCP4822_device dev, float value, MCP4822_OUTPUT_CONFIG config);
 
 
 #endif
