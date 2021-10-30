36238b17aa5f8fece6322b5f79594d6d258c80f7
Adam Siemasz <asiemasz1@gmail.com>
2021-10-30 19:09:00 +0200

    add assertions for values to dac

--- MCP4822.c
+++ MCP4822.c
@@ -1,10 +1,12 @@
 #include "MCP4822.h"
+#include <assert.h>
 
 void MCP4822_setValue(const MCP4822_device* dev,const uint16_t value,const MCP4822_OUTPUT_CONFIG* config) {
+    assert(value < 4096);
     dev->CSenableFunction(dev->SS_Port, dev->SS_pin);
 
     uint8_t data[2];
-    data[0] = (config->_config << 4) | ((value << 8) & 0xF);
+    data[0] = (config->_config << 4) | ((value >> 8) & 0xF);
     data[1] = value & 0xFF;
 
     dev->writeFunction(data, 2);
@@ -13,8 +15,13 @@ void MCP4822_setValue(const MCP4822_device* dev,const uint16_t value,const MCP48
 }
 
 void MCP4822_setVoltage(const MCP4822_device* dev,const float value,const MCP4822_OUTPUT_CONFIG* config) {
-    uint16_t rawVal = value * 4096 / (REF_VOLTAGE * config->gain); 
-    MCP48222_setValue(dev, rawVal, config);
+    assert(value >= 0);
+    uint16_t rawVal;
+    if(value > 2.048 * (2 - config->gain)) 
+        rawVal = 4095;
+    else
+        rawVal = value * 4096 / (REF_VOLTAGE * (2 - config->gain)); 
+    MCP4822_setValue(dev, rawVal, config);
 }
 
 
